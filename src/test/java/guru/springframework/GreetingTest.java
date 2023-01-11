72548e8f5d63246d2d9d4156e2ce57dd74eb6a1a
ATTOLODE <vincent.attolode@worldline.com>
2023-01-11 19:27:17 +0100

    junit lifecycle demo

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,4 +1,39 @@
-import static org.junit.jupiter.api.Assertions.*;
+package guru.springframework;
+
+import org.junit.jupiter.api.*;
+
 class GreetingTest {
-  
+
+    Greeting greeting;
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Before - I am only called once!");
+    }
+
+    @BeforeEach
+    void methodInit() {
+        System.out.println("In before each...");
+        greeting = new Greeting();
+    }
+
+    @Test
+    void helloWorld() {
+        System.out.println(greeting.helloWorld());
+    }
+
+    @Test
+    void testHelloWorld() {
+        System.out.println(greeting.helloWorld("Macodou"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("In after each...");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After!! ******* Game over, YOLO !");
+    }
 }
